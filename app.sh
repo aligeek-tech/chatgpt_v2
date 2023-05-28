f8728252018ede7beea612573c29b752755fabf8
Ali Jalali <admin@example.com>
2023-05-29 01:25:31 +0330

    update

--- app.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker compose -f docker-compose.yml build --no-cache && docker compose -f docker-compose.yml up -d && docker system prune -a -f && docker image prune -a -f && docker container restart $(docker ps -aq)
